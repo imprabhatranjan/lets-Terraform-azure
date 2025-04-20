5b6029840b732105f64aa56629c2ff9cddf48a4b
Prahat Ranjan Choudhary <imprabhatranjan@gmail.com>
2025-04-20 11:42:31 +0530

    variable declaration

--- dhondhu_infra/child_modules/azurerm_resource_group/main.tf
+++ dhondhu_infra/child_modules/azurerm_resource_group/main.tf
@@ -1,4 +1,8 @@
+variable "rg_name" {}
+
+variable "rg_location" {}
+
 resource "azurerm_resource_group" "rg" {
-  name = "rg-kuchbhi"
-  location = "eastus"
+  name = var.rg_name
+  location = var.rg_location
 }
--- dhondhu_infra/parent_module/dev/main.tf
+++ dhondhu_infra/parent_module/dev/main.tf
@@ -3,6 +3,6 @@ module "resource_group" {
 }
 
 module "storage_account" {
-  depends_on = [ module.resource_group ]
-  source = "../../child_modules/azure_storage_account"
+  depends_on = [module.resource_group]
+  source     = "../../child_modules/azure_storage_account"
 }
